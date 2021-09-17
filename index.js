a11ca4b3dfcdb4287b11fac0e103f9ed8707972a
SadboiTay <tg.hakes@gmail.com>
2021-09-17 15:39:17 -0600

    initialize question prompt now functioning

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('.utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -135,7 +136,7 @@ const questions = [
 // TODO: Create a function to initialize app
 const init = () => {
     return inquirer.prompt(questions)
-    .then(console.log(readmeData))
+    .then(readmeData => console.log(readmeData))
     .catch(err => {
         console.log(err);
     });
